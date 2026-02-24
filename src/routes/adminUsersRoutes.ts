8188d273448297b3208b37c2bd24ecac6d370b59
Kingsley-codes <agbamkingsley@gmail.com>
2026-02-24 22:34:46 +0100

    bugs

--- src/routes/adminUsersRoutes.ts
+++ src/routes/adminUsersRoutes.ts
@@ -1,29 +1,15 @@
 import express from "express";
 import {
-  getAllProducers,
   getAllFarmers,
-  verifyProducer,
   suspendFarmer,
   activateFarmer,
-  suspendProducer,
-  activateProducer,
 } from "../controllers/adminUsersController.js";
 import { adminAuthenticate } from "../middleware/authenticationMiddleware.js";
 
 const adminUsersRouter = express.Router();
-
-adminUsersRouter.get("/producers", adminAuthenticate, getAllProducers);
-adminUsersRouter.post("/producers/verify", adminAuthenticate, verifyProducer);
-adminUsersRouter.post("/producers/suspend", adminAuthenticate, suspendProducer);
-adminUsersRouter.post(
-  "/producers/activate",
-  adminAuthenticate,
-  activateProducer,
-);
 
 adminUsersRouter.get("/farmers", adminAuthenticate, getAllFarmers);
 adminUsersRouter.post("/farmers/suspend", adminAuthenticate, suspendFarmer);
 adminUsersRouter.post("/farmers/activate", adminAuthenticate, activateFarmer);
 
 export default adminUsersRouter;
-
