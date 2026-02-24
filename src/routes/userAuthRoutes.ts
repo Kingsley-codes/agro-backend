c0047c001993d3f1995c2204a1ca27c4be1c958c
Kingsley-codes <agbamkingsley@gmail.com>
2026-02-24 22:35:44 +0100

    new bugs

--- src/routes/userAuthRoutes.ts
+++ src/routes/userAuthRoutes.ts
@@ -1,27 +1,10 @@
 import express from "express";
-import {
-  registerUser,
-  login,
-  registerProducer,
-  loginProducer,
-  onboardProducer,
-} from "../controllers/authControllers.js";
-import { uploadProducerImages } from "../middleware/uploadMiddleware.js";
+import { registerUser, login } from "../controllers/authControllers.js";
 
 const userAuthRouter = express.Router();
 
 userAuthRouter.post("/register", registerUser); // User Registration routes
 
 userAuthRouter.post("/login", login); // User Login route
-
-userAuthRouter.post("/producer/register", registerProducer); // producer Registration routes
-
-userAuthRouter.post("/producer/login", loginProducer); // producer Login route
-
-userAuthRouter.post(
-  "/producer/onboarding",
-  uploadProducerImages,
-  onboardProducer,
-); // producer onboarding route
 
 export default userAuthRouter;
